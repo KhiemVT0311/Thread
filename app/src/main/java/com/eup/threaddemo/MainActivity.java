219abdb01116959677838e91782a8a16584f60a7
Vũ Trọng Khiêm <phamthihuong03111999@gmail.com>
2021-10-11 21:37:10 +0700

    Thread

--- app/src/main/java/com/eup/threaddemo/MainActivity.java
+++ app/src/main/java/com/eup/threaddemo/MainActivity.java
@@ -7,72 +7,53 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.eup.threaddemo.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-     public static final  int MESSAGE_COUNTDOWN = 1000;
-    private static final int MESSAGE_DONE = 1002;
+public class MainActivity extends AppCompatActivity{
     private ActivityMainBinding binding;
-    private Handler mhandler;
+    private static final String TAG = "MainActivity";
+
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
-        initView();
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 
-    private void initView() {
-        binding.btnCount.setOnClickListener(this);
-        mhandler = new Handler(){
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                switch (msg.what){
-                    case MESSAGE_COUNTDOWN:
-                        binding.tvTimer.setText(String.valueOf(msg.arg1));
-                        break;
-                    case MESSAGE_DONE:
-                        Toast.makeText(MainActivity.this,"Done!!!",Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        break;
+    public void startThread(View view) {
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+    }
+
+    public void stopThread(View view) {
+        stopThread = true;
+    }
+
+    class ExampleRunnable implements Runnable {
+        int seconds;
+
+        ExampleRunnable(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < seconds; i++) {
+                if (stopThread)
+                    return;
+                Log.d(TAG, "startThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
             }
-        };
+        }
     }
-
-    @Override
-    public void onClick(View view) {
-    doCounDown();
-    }
-
-    private void doCounDown() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int time = 10;
-                do {
-                    time --;
-                    Message message = new Message();
-                    message.what = MESSAGE_COUNTDOWN;
-                    message.arg1 = time;
-                    mhandler.sendMessage(message);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex){
-                        ex.printStackTrace();
-                    }
-                } while (time>0);
-                mhandler.sendEmptyMessage(MESSAGE_DONE);
-            }
-
-        });
-           thread.start();
-
-    }
-
 }
